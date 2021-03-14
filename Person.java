0dfbfa6529a28a2b015194486b44f928cb541dbc
Angelica <Angelica137@users.noreply.github.com>
2021-03-14 17:34:10 +0000

    add name and date

--- Person.java
+++ Person.java
@@ -2,41 +2,36 @@
 /**
  * To demonstrate boolean.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Angelica Beristain
+ * @version 14 March 2021
  */
-public class Person
-{
+public class Person {
 	double cmHeight;
 	double kgWeight;
 	boolean smoker = true;
 	int weeklyAlcoholUnits = 50; // 1 pint = 2 units, 1 short = 1 unit)
-	final int RWAL = 21;   //RWAL - Recommended Weekly Alcohol Limit
+	final int RWAL = 21; // RWAL - Recommended Weekly Alcohol Limit
 	int restPulse;
 
-	public boolean checkPulse()
-	{
-	   boolean healthyPulse;
-	   healthyPulse = ((restPulse > 30) && (restPulse < 180));
-	   return healthyPulse;
+	public boolean checkPulse() {
+		boolean healthyPulse;
+		healthyPulse = ((restPulse > 30) && (restPulse < 180));
+		return healthyPulse;
 	}
-	
-	public boolean checkAbuser()
-	{
-	      boolean abuser ;
-	      abuser = ((weeklyAlcoholUnits > RWAL) || (smoker) );
-	      return abuser;
+
+	public boolean checkAbuser() {
+		boolean abuser;
+		abuser = ((weeklyAlcoholUnits > RWAL) || (smoker));
+		return abuser;
 	}
-	
-	public void displayProfile()
-	{
-	   System.out.println("………Health Profile……");
-	   
-	   System.out.println("Healthy pulse check = " + checkPulse());
-	   System.out.print("Abusing Body = " + checkAbuser());
-	   
+
+	public void displayProfile() {
+		System.out.println("ï¿½ï¿½ï¿½Health Profileï¿½ï¿½");
+
+		System.out.println("Healthy pulse check = " + checkPulse());
+		System.out.print("Abusing Body = " + checkAbuser());
+
 	}
-	//Put curseAndSwear method here
-	
-	
+	// Put curseAndSwear method here
+
 }
